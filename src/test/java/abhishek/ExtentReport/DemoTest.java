ba426520621821a577854c1748f2accddf418c03
AbhishekBallullayaK <abhishekbk1806@gmail.com>
2025-12-24 18:08:10 +0530

    Another commit from demo

--- src/test/java/abhishek/ExtentReport/DemoTest.java
+++ src/test/java/abhishek/ExtentReport/DemoTest.java
@@ -8,4 +8,16 @@ public class DemoTest {
 	public void demoTest() {
 		System.out.println("This is demo test");
 	}
+	
+	public void demoTest1() {
+		System.out.println("This is demo test1");
+	}
+	
+	public void demoTest2() {
+		System.out.println("This is demo test2");
+	}
+	
+	public void demoTest3() {
+		System.out.println("This is demo test3");
+	}
 }
---
+++ src/test/java/abhishek/ExtentReport/DemoTest2.java
@@ -0,0 +1,23 @@
+package abhishek.ExtentReport;
+
+import org.testng.annotations.Test;
+
+public class DemoTest2 {
+
+	@Test
+	public void demoTest() {
+		System.out.println("This is demo test02");
+	}
+	
+	public void demoTest1() {
+		System.out.println("This is demo test12");
+	}
+	
+	public void demoTest2() {
+		System.out.println("This is demo test22");
+	}
+	
+	public void demoTest3() {
+		System.out.println("This is demo test32");
+	}
+}
